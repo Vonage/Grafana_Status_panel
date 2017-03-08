293a78a32ded3545cd7685be0b23468bc6e32ec0
Yochpaz Yehonatan <Yochpaz.Yehonatan@vonage.com>
2017-03-08 11:49:37 +0200

    * Add option to make panel disabled by a certain metric * Add option to add annotation on the top right of a panel by a certain metric

--- src/status_ctrl.js
+++ src/status_ctrl.js
@@ -8,189 +8,227 @@ import coreModule from "app/core/core_module"
 import './css/status_panel.css!';
 
 export class StatusPluginCtrl extends MetricsPanelCtrl {
-  /** @ngInject */
-  constructor($scope, $injector, $log, $filter, annotationsSrv) {
-    super($scope, $injector);
+    /** @ngInject */
+    constructor($scope, $injector, $log, $filter, annotationsSrv) {
+        super($scope, $injector);
 
-    //this.log = $log.debug;
-    this.filter = $filter;
+        //this.log = $log.debug;
+        this.filter = $filter;
 
-    this.aggregations = ['Last', 'First', 'Max', 'Min', 'Sum', 'Avg'];
+        this.statusTypes = ['Threshold', 'Disable State', 'Annotation'];
+        this.aggregations = ['Last', 'First', 'Max', 'Min', 'Sum', 'Avg'];
 
-    /** Bind events to functions **/
-    this.events.on('render', this.onRender.bind(this));
-    this.events.on('refresh', this.postRefresh.bind(this));
-    this.events.on('data-error', this.onDataError.bind(this));
-    this.events.on('data-received', this.onDataReceived.bind(this));
-    this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
-    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
-  }
-
-  postRefresh() {
-    this.measurements = this.panel.targets;
-
-    /** Duplicate alias validation **/
-    this.duplicates = false;
-
-    this.measurements = _.filter(this.measurements, (measurement) => {
-      return !measurement.hide;
-    });
-
-    _.each(this.measurements, (m) => {
-      let res = _.filter(this.measurements, (measurement) => {
-        return (m.alias == measurement.alias || (m.target == measurement.target && m.target)) && !m.hide;
-      });
-
-      if (res.length > 1) {
-        this.duplicates = true;
-      }
-    });
-  }
-
-  onInitEditMode() {
-    this.addEditorTab('Options', 'public/plugins/vonage-status-panel/editor.html', 2);
-  }
-
-  setElementHeight() {
-    this.$panelContainer.find('.status-panel').css('height', this.$panelContoller.height + 'px');
-  }
-
-  onRender() {
-    this.setElementHeight();
-
-    if (this.panel.clusterName) {
-      this.panel.displayName =
-        this.filter('interpolateTemplateVars')(this.panel.clusterName, this.$scope)
-          .replace(new RegExp(this.panel.namePrefix, 'i'), '');
-    } else {
-      this.panel.displayName = "";
+        /** Bind events to functions **/
+        this.events.on('render', this.onRender.bind(this));
+        this.events.on('refresh', this.postRefresh.bind(this));
+        this.events.on('data-error', this.onDataError.bind(this));
+        this.events.on('data-received', this.onDataReceived.bind(this));
+        this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
+        this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
     }
 
+    postRefresh() {
+        this.measurements = this.panel.targets;
 
+        /** Duplicate alias validation **/
+        this.duplicates = false;
 
-    let targets = this.panel.targets;
+        this.measurements = _.filter(this.measurements, (measurement) => {
+            return !measurement.hide;
+        });
 
-    this.crit = [];
-    this.warn = [];
-    this.display = [];
+        _.each(this.measurements, (m) => {
+            let res = _.filter(this.measurements, (measurement) => {
+                return (m.alias == measurement.alias || (m.target == measurement.target && m.target)) && !m.hide;
+            });
 
-    _.each(this.series, (s) => {
-      let target = _.find(targets, (target) => {
-        return target.alias == s.alias || target.target == s.alias;
-      });
+            if (res.length > 1) {
+                this.duplicates = true;
+            }
+        });
+    }
 
-      if (!target) {
-        return;
-      }
+    onInitEditMode() {
+        this.addEditorTab('Options', 'public/plugins/vonage-status-panel/editor.html', 2);
+    }
 
-      s.thresholds = StatusPluginCtrl.parseThresholds(target);
-      s.inverted = s.thresholds.crit < s.thresholds.warn;
-      s.display = target.display;
-      s.alias = target.alias;
-      s.url = target.url;
+    setElementHeight() {
+        this.$panelContainer.find('.status-panel').css('height', this.$panelContoller.height + 'px');
+    }
 
-      let value;
+    onRender() {
+        this.setElementHeight();
 
-      switch (target.aggregation) {
-        case 'Max':
-          value = s.stats.max;
-          break;
-        case 'Min':
-          value = s.stats.min;
-          break;
-        case 'Sum':
-          value = s.stats.total;
-          break;
-        case 'Avg':
-          value = s.stats.avg;
-          break;
-        case 'First':
-          value = s.datapoints[0][0];
-          break;
-        default:
-          value = s.datapoints[s.datapoints.length - 1][0];
-      }
-
-      s.display_value = value;
-
-      if (!s.inverted) {
-        if (value >= s.thresholds.crit) {
-          this.crit.push(s);
-        } else if (value >= s.thresholds.warn) {
-          this.warn.push(s);
-        } else if (s.display) {
-          this.display.push(s);
+        if (this.panel.clusterName) {
+            this.panel.displayName =
+                this.filter('interpolateTemplateVars')(this.panel.clusterName, this.$scope)
+                    .replace(new RegExp(this.panel.namePrefix, 'i'), '');
+        } else {
+            this.panel.displayName = "";
         }
-      } else {
-        if (value <= s.thresholds.crit) {
-          this.crit.push(s);
-        } else if (value <= s.thresholds.warn) {
-          this.warn.push(s);
-        } else if (s.display) {
-          s.display_value = value;
-          this.display.push(s);
+
+
+
+        let targets = this.panel.targets;
+
+        this.crit = [];
+        this.warn = [];
+        this.display = [];
+        this.disabled = [];
+        this.annotation = []
+
+        _.each(this.series, (s) => {
+            let target = _.find(targets, (target) => {
+                return target.alias == s.alias || target.target == s.alias;
+            });
+
+            if (!target) {
+                return;
+            }
+
+            s.alias = target.alias;
+            s.url = target.url;
+
+            let value;
+            switch (target.aggregation) {
+                case 'Max':
+                    value = _.max(s.datapoints, (point) => { return point[0]; })[0];
+                    value = s.stats.max;
+                    break;
+                case 'Min':
+                    value = _.min(s.datapoints, (point) => { return point[0]; })[0];
+                    value = s.stats.min;
+                    break;
+                case 'Sum':
+                    value = 0;
+                    _.each(s.datapoints, (point) => { value += point[0] });
+                    value = s.stats.total;
+                    break;
+                case 'Avg':
+                    value = s.stats.avg;
+                    break;
+                case 'First':
+                    value = s.datapoints[0][0];
+                    break;
+                default:
+                    value = s.datapoints[s.datapoints.length - 1][0];
+            }
+
+            s.display_value = value;
+
+            if (target.statusType == "Threshold") {
+                this.handleThresholdStatus(s, target);
+            }
+            else if (target.statusType == "Disable State") {
+                this.handleDisabledStatus(s,target);
+            }
+            else if (target.statusType == "Annotation") {
+                this.handleAnnotations(s, target);
+            }
+        });
+
+        this.handle_css_display();
+        this.parseUri();
+    }
+
+    handleThresholdStatus(series, target) {
+        series.thresholds = StatusPluginCtrl.parseThresholds(target);
+        series.inverted = series.thresholds.crit < series.thresholds.warn;
+        series.display = target.display;
+
+        if (!series.inverted) {
+            if (series.display_value >= series.thresholds.crit) {
+                this.crit.push(series);
+            } else if (series.display_value >= series.thresholds.warn) {
+                this.warn.push(series);
+            } else if (series.display) {
+                this.display.push(series);
+            }
+        } else {
+            if (series.display_value <= series.thresholds.crit) {
+                this.crit.push(series);
+            } else if (series.display_value <= series.thresholds.warn) {
+                this.warn.push(series);
+            } else if (series.display) {
+                this.display.push(series);
+            }
         }
-      }
-    });
-
-    this.$panelContainer.removeClass('error-state warn-state ok-state gray-state');
-
-    if (this.crit.length > 0 || this.duplicates) {
-      this.$panelContainer.addClass('error-state');
-    } else if (this.warn.length > 0) {
-      this.$panelContainer.addClass('warn-state');
-    } else if(this.series.length == 0 && this.panel.isGrayColor) {
-      this.$panelContainer.addClass('gray-state');
-    } else {
-      this.$panelContainer.addClass('ok-state');
     }
 
-    this.parseUri();
-  }
+    handleDisabledStatus(series, target) {
 
-  parseUri() {
-    if (this.panel.links && this.panel.links.length > 0) {
-      this.uri = this.panel.links[0].dashUri + "?" + this.panel.links[0].params;
-    } else {
-      this.uri = undefined;
+        series.disabledValue = target.disabledValue;
+
+        if (series.display_value == series.disabledValue) {
+            this.disabled.push(series);
+        }
     }
-  }
 
-  static parseThresholds(measurement) {
-    let res = {};
+    handleAnnotations(series, target) {
+        this.annotation.push(series);
+    }
 
-    res.warn = measurement.warn;
-    res.crit = measurement.crit;
+    handle_css_display() {
+        this.$panelContainer.removeClass('error-state warn-state disabled-state ok-state no-data-state');
 
-    return res;
-  }
+        if(this.duplicates) {
+            this.$panelContainer.addClass('error-state');
+        } else if (this.disabled.length > 0) {
+            this.$panelContainer.addClass('disabled-state');
+        } else if (this.crit.length > 0) {
+            this.$panelContainer.addClass('error-state');
+        } else if (this.warn.length > 0) {
+            this.$panelContainer.addClass('warn-state');
+        } else if(this.series.length == 0 && this.panel.isGrayOnNoData) {
+            this.$panelContainer.addClass('no-data-state');
+        } else {
+            this.$panelContainer.addClass('ok-state');
+        }
+    }
 
-  onDataReceived(dataList) {
-    this.series = dataList.map(this.seriesHandler.bind(this));
+    parseUri() {
+        if (this.panel.links && this.panel.links.length > 0) {
+            this.uri = this.panel.links[0].dashUri + "?" + this.panel.links[0].params;
+        } else {
+            this.uri = undefined;
+        }
+    }
 
-    this.render();
-  }
+    static parseThresholds(metricOptions) {
+        let res = {};
 
-  onDataError() {
-    this.crit = [];
-    this.warn = [];
-  }
+        res.warn = metricOptions.warn;
+        res.crit = metricOptions.crit;
 
-  seriesHandler(seriesData) {
-    var series = new TimeSeries({
-      datapoints: seriesData.datapoints,
-      alias: seriesData.target
-    });
+        return res;
+    }
 
-    series.flotpairs = series.getFlotPairs("connected");
+    onDataReceived(dataList) {
+        this.series = dataList.map(this.seriesHandler.bind(this));
 
-    return series;
-  }
+        this.render();
+    }
 
-  link(scope, elem, attrs, ctrl) {
-    this.$panelContainer = elem.find('.panel-container');
-    this.$panelContoller = ctrl;
-  }
+    onDataError() {
+        this.crit = [];
+        this.warn = [];
+    }
+
+    seriesHandler(seriesData) {
+        var series = new TimeSeries({
+            datapoints: seriesData.datapoints,
+            alias: seriesData.target
+        });
+
+        series.flotpairs = series.getFlotPairs("connected");
+
+        return series;
+    }
+
+    link(scope, elem, attrs, ctrl) {
+        this.$panelContainer = elem.find('.panel-container');
+        this.$panelContoller = ctrl;
+    }
 }
 
 StatusPluginCtrl.templateUrl = 'module.html';
